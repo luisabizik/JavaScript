e783679b818c65bcb6581bf055dd6ee2221fd1ff
luisabizik <118220859+luisabizik@users.noreply.github.com>
2022-12-14 18:03:08 +0100

    Create DogYears.js

---
+++ DogYears.js
@@ -0,0 +1,23 @@
+// assigning a constant value to myAge
+const myAge = 27;
+
+// assigning a variable to earlyYears
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+// subtraction to myAge and saving the value in a new variable laterYears
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+// saving my age in dog years in a variable
+let myAgeInDogYears = earlyYears + laterYears;
+
+myName= 'Luisa'.toLowerCase();
+
+// prints the statement that display my name and the age in dog years
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
