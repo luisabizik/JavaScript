3cfb6e3b04fb259a9afc5838856ce29aa25a5ecc
luisabizik <luisa.bizik@gmail.com>
2023-01-11 19:18:21 +0100

    finished

--- RaceDay.js
+++ RaceDay.js
@@ -1,14 +1,27 @@
 
-
+//race numbers assigned randomly
 let raceNumber = Math.floor(Math.random() * 1000);
 
-let registeredEarly = true;
-let runnerAge = 27;
+//checks if the runner is registered early or not and his/her age
+let registeredEarly = false;
+let runnerAge = 18;
 
+//control flow statement that checks whether the runner is an adult and registered early
 if (runnerAge >= 18 && registeredEarly === true) {
   raceNumber += 1000;
 } 
 
-if (runnerAge >= 18 && registeredEarly === true) {
-  console.log(`${raceNumber}` 'will race at 9:30 am');
+//checks the runners on different conditions
+if (runnerAge > 18 && registeredEarly === true) {
+  console.log(`${raceNumber}`);
+  console.log('will race at 9:30 am');
+} else if (runnerAge > 18 && registeredEarly === false) {
+  console.log(`${raceNumber}`);
+  console.log('will race at 11:00 am');
+} else if (runnerAge < 18) {
+  console.log(`${raceNumber}`);
+  console.log('will race at 12:30 pm (regardless of registration)');
+} else if (runnerAge === 18) {
+  console.log(`${raceNumber}`);
+  console.log('Please, come at the registration desk.');
 }
